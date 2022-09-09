892048a39b5dca4adc9d83c48aeb5e9c8416dd4d
emiruyan <emir-uyn@hotmail.com>
2022-09-09 15:38:14 +0300

    Ball Controller
    We moved the ball and made its color change randomly as it collides.

---
+++ BouncingDot/Assets/Scripts/BallController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BallController : MonoBehaviour
+{
+   [SerializeField] private Rigidbody2D rb;
+   [SerializeField] private SpriteRenderer sr;
+   [SerializeField] private Sprite[] _ballSprite;//Topun renklerini atayacağımız dizi.
+   [SerializeField] private float _jumpForce;
+   [SerializeField] private string _currentColor;
+
+
+   private void Start()
+   {
+      RandomBall(3);//Oyun içerisinde ilk rengimiz yeşil olduğu için burada parametreyi "Green" verdik.
+   }
+   
+   public void RandomBall(int index) //Topa rastgele renk vereceğimiz method:
+   {
+      switch (index)
+      {
+         case 0: _currentColor = "Red";
+            sr.sprite = _ballSprite[0];
+            gameObject.tag = "Red";//Top belirttiğimiz index'i alırsa tag'ı o indexe göre değişiyor.
+            break;
+         case 1: _currentColor = "Yellow";
+            sr.sprite = _ballSprite[1];
+            gameObject.tag = "Yellow";
+            break;
+         case 2: _currentColor = "Blue";
+            sr.sprite = _ballSprite[2];
+            gameObject.tag = "Blue";
+            break;
+         case 3: _currentColor = "Green";
+            sr.sprite = _ballSprite[3];
+            gameObject.tag = "Green";
+            break;
+         case 4: _currentColor = "Purple";
+            sr.sprite = _ballSprite[4];
+            gameObject.tag = "Purple";
+            break;
+         case 5: _currentColor = "Orange";
+            sr.sprite = _ballSprite[5];
+            gameObject.tag = "Orange";
+            break;
+      }
+   }
+   
+   private void OnCollisionEnter2D(Collision2D other)//Çarpışma olduğunda topa güç uyguluyoruz.
+   {
+      rb.velocity = Vector3.up * _jumpForce;//Vector3.up ile jumpForce değerini velocity'e atadık.
+      if (other.gameObject.tag != _currentColor)//
+      {
+         
+      }
+      else
+      {
+         int _randomNumber = Random.Range(0, 6);
+         RandomBall(_randomNumber);
+      }
+   }
+}
